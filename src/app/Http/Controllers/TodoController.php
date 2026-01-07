5523e2f8752fb79f99e29c60e9364aa87b51dc16
yuki <raiku.yuki@icloud.com>
2026-01-07 22:23:10 +0900

    todoアプリ

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,21 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TodoRequest;
 use App\Models\Todo;
-
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        $todos = Todo::all();
+        $todos = Todo::with('category')->get();
+        $categories = Category::all();
 
-        return view('index', compact('todos'));
+        return view('index', compact('todos', 'categories'));
     }
 
     public function store(TodoRequest $request)
     {
-        $todo = $request->only(['content']);
+        $todo = $request->only(['category_id', 'content']);
         Todo::create($todo);
 
         return redirect('/')->with('message', 'Todoを作成しました');
@@ -36,5 +37,13 @@ class TodoController extends Controller
     {
         Todo::find($request->id)->delete();
         return redirect('/')->with('message', 'Todoを削除しました');
+    }
+
+    public function search(Request $request)
+    {
+        $todos = Todo::with('category')->CategorySearch($request->category_id)->KeywordSearch($request->keyword)->get();
+        $categories = Category::all();
+
+        return view('index', compact('todos', 'categories'));
     }
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\TodoController;
+use App\Http\Controllers\CategoryController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,3 +22,13 @@ Route::post('/todos', [TodoController::class, 'store']);
 Route::patch('/todos/update', [TodoController::class, 'update']);
 
 Route::delete('/todos/delete', [TodoController::class, 'destroy']);
+
+Route::get('/todos/search', [TodoController::class, 'search']);
+
+Route::get('/categories', [CategoryController::class, 'index']);
+
+Route::post('/categories', [CategoryController::class, 'store']);
+
+Route::patch('/categories/update', [CategoryController::class, 'update']);
+
+Route::delete('/categories/delete', [CategoryController::class, 'destroy']);
